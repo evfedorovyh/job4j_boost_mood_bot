4760621f670ddfc5bdf04505ad3ea6c591638717
evfedorovyh <jhon2002@mail.ru>
2025-02-06 17:59:22 +0300

    Aware интерфейсы [#505301]

--- src/main/java/ru/job4j/bmb/services/AchievementService.java
+++ src/main/java/ru/job4j/bmb/services/AchievementService.java
@@ -1,11 +1,18 @@
-package ru.job4j.bmb.logic;
+package ru.job4j.bmb.services;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ReminderService {
+public class AchievementService implements BeanNameAware {
+
+    @Override
+    public void setBeanName(String beanName) {
+        System.out.println(beanName);
+    }
+
     @PostConstruct
     public void init() {
         System.out.println("Bean is going through @PostConstruct init.");
