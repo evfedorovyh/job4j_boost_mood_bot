5c22ba820ecc61295bdbcd4b406099c748ef8c6a
evfedorovyh <jhon2002@mail.ru>
2025-02-10 22:51:35 +0300

    @Autowired | @Qualifier [#505314]

--- src/test/java/ru/job4j/bmb/services/MoodLogServiceTest.java
+++ src/test/java/ru/job4j/bmb/services/MoodLogServiceTest.java
@@ -36,7 +36,7 @@ class MoodLogServiceTest {
 
     @Test
     public void whenUserFound() {
-        userRepository.save(new User( 1L,100, 100));
+        userRepository.save(new User(1L, 100, 100));
         userRepository.save(new User(2L, 200, 200));
         userRepository.save(new User(3L, 300, 300));
         var startOfDay = LocalDate.now()
@@ -48,17 +48,17 @@ class MoodLogServiceTest {
                 .atStartOfDay(ZoneId.systemDefault())
                 .toInstant()
                 .toEpochMilli() - 1;
-        moodLogRepository.save(new MoodLog(1L,new User(1L,100, 100),
+        moodLogRepository.save(new MoodLog(1L,new User(1L, 100, 100),
                 new Mood("good", true),
                 LocalDateTime.now()
                         .minusDays(10).atZone(ZoneId.systemDefault())
                         .toInstant().toEpochMilli()));
-        moodLogRepository.save(new MoodLog(2L, new User(2L,200, 200),
+        moodLogRepository.save(new MoodLog(2L, new User(2L, 200, 200),
                 new Mood("good", true),
                 LocalDateTime.now()
                         .minusDays(1).atZone(ZoneId.systemDefault())
                         .toInstant().toEpochMilli()));
-        moodLogRepository.save(new MoodLog(3L, new User(3L,300, 300),
+        moodLogRepository.save(new MoodLog(3L, new User(3L, 300, 300),
                 new Mood("good", true),
                 LocalDate.now().atStartOfDay(ZoneId.systemDefault())
                         .plusHours(2)
@@ -67,34 +67,34 @@ class MoodLogServiceTest {
         List<User> users = moodLogService.findUsersWhoDidNotVoteToday(startOfDay, endOfDay);
         List<User> expected = List.of(
                 new User(1L, 100, 100),
-                new User(2L,200, 200));
+                new User(2L, 200, 200));
         assertTrue(users.containsAll(expected));
     }
 
     @Test
     public void daysWithGoodMoodIs3() {
-        User userOne = new User( 1L,100, 100);
-        moodLogRepository.save(new MoodLog(1L,new User(1L,100, 100),
+        User userOne = new User(1L, 100, 100);
+        moodLogRepository.save(new MoodLog(1L,new User(1L, 100, 100),
                 new Mood("good", true),
                 LocalDateTime.now()
                         .minusDays(5).atZone(ZoneId.systemDefault())
                         .toInstant().toEpochMilli()));
-        moodLogRepository.save(new MoodLog(2L,new User(1L,100, 100),
+        moodLogRepository.save(new MoodLog(2L,new User(1L, 100, 100),
                 new Mood("good", false),
                 LocalDateTime.now()
                         .minusDays(4).atZone(ZoneId.systemDefault())
                         .toInstant().toEpochMilli()));
-        moodLogRepository.save(new MoodLog(3L,new User(1L,100, 100),
+        moodLogRepository.save(new MoodLog(3L,new User(1L, 100, 100),
                 new Mood("good", true),
                 LocalDateTime.now()
                         .minusDays(3).atZone(ZoneId.systemDefault())
                         .toInstant().toEpochMilli()));
-        moodLogRepository.save(new MoodLog(4L,new User(1L,100, 100),
+        moodLogRepository.save(new MoodLog(4L,new User(1L, 100, 100),
                 new Mood("good", true),
                 LocalDateTime.now()
                         .minusDays(2).atZone(ZoneId.systemDefault())
                         .toInstant().toEpochMilli()));
-        moodLogRepository.save(new MoodLog(5L,new User(2L,200, 200),
+        moodLogRepository.save(new MoodLog(5L,new User(2L, 200, 200),
                 new Mood("good", true),
                 LocalDateTime.now()
                         .minusDays(1).atZone(ZoneId.systemDefault())
@@ -106,27 +106,27 @@ class MoodLogServiceTest {
     @Test
     public void daysWithGoodMoodIs10() {
         User userTwo = new User(2L, 200, 200);
-        moodLogRepository.save(new MoodLog(1L,new User(1L,100, 100),
+        moodLogRepository.save(new MoodLog(1L,new User(1L, 100, 100),
                 new Mood("good", true),
                 LocalDateTime.now()
                         .minusDays(5).atZone(ZoneId.systemDefault())
                         .toInstant().toEpochMilli()));
-        moodLogRepository.save(new MoodLog(2L,new User(1L,100, 100),
+        moodLogRepository.save(new MoodLog(2L,new User(1L, 100, 100),
                 new Mood("good", false),
                 LocalDateTime.now()
                         .minusDays(4).atZone(ZoneId.systemDefault())
                         .toInstant().toEpochMilli()));
-        moodLogRepository.save(new MoodLog(3L,new User(1L,100, 100),
+        moodLogRepository.save(new MoodLog(3L,new User(1L, 100, 100),
                 new Mood("good", true),
                 LocalDateTime.now()
                         .minusDays(3).atZone(ZoneId.systemDefault())
                         .toInstant().toEpochMilli()));
-        moodLogRepository.save(new MoodLog(4L,new User(1L,100, 100),
+        moodLogRepository.save(new MoodLog(4L,new User(1L, 100, 100),
                 new Mood("good", true),
                 LocalDateTime.now()
                         .minusDays(2).atZone(ZoneId.systemDefault())
                         .toInstant().toEpochMilli()));
-        moodLogRepository.save(new MoodLog(5L,new User(2L,200, 200),
+        moodLogRepository.save(new MoodLog(5L,new User(2L, 200, 200),
                 new Mood("good", true),
                 LocalDateTime.now()
                         .minusDays(10).atZone(ZoneId.systemDefault())
